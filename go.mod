2431e961b32e801f53b3686a7e20df677a953e5a
paulo-souza <phespecialmaster@gmail.com>
2024-10-10 16:45:58 -0300

    2ROro-Primeiro commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/paulo-souza/learn_golang
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/paulo-souza/learn_golang/lib"
+)
+
+func main() {
+	fmt.Println(lib.CaraEuSouDemais())
+}
